6ac2fa7e59c8840230c702ad80afb48446e74fd2
Pedro palacios <pedro_gpa@hotmail.com>
2014-01-21 22:53:23 -0500

    Agregado comunicador.py

--- pc/recopilador.py
+++ pc/recopilador.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
-
-import sys
-sys.path.append("/home/amd/Ubuntu One/python/modulos")
-sys.path.append("/home/pedro/Ubuntu One/python/modulos")
-
-#-------------------------------------------------------
