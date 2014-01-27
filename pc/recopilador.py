ec8ac856a60e9ea112a8e6ae60e37aaecde4aa00
Pedro Palacios <pedro_gpa@hotmail.com>
2014-01-27 00:00:07 -0500

    Método agregado: 'terminar'. Le indica al thread que debe terminar. Se renombro el método '__run__' por el correcto 'run'.

--- pc/recopilador.py
+++ pc/recopilador.py
@@ -4,19 +4,19 @@
 from __future__ import print_function
 import threading
 
-class Recopilador(threading.Theading):
+class Recopilador(threading.Thread):
     """Se encarga de leer y almacenar los datos recibidos"""
 
     def __init__(self,bt_socket,cola,separador=';',traducir=None,volcar_datos=None):
-
+        threading.Thread.__init__(self)
         #Tratamos la conexión como un archivo de texto
-        stream = bt_socket.makefile("r")
+        self.stream = bt_socket.makefile("r")
 
         self.lista=cola
         self.sep = separador
 
         #Por defecto, no se traducen los datos
-        if ( traducir=None ):
+        if ( traducir == None ):
             self.traducir = lambda x:x
         else:
             self.traducir = traducir
@@ -28,23 +28,31 @@ class Recopilador(threading.Theading):
         else:
             self.volcar_datos = volcar_datos
 
+    def terminar(self):
+        self.activo = False
 
-    def __run__(self):
-        while true:
+    def run(self):
+        self.activo=True
+        print("Recopilador funcionando")
+        while self.activo:
+            #print("Inicia bucle")
             #Cadena donde se almacena una lectura incompleta
-            aux_cad = string()
+            aux_cad = ""
             
             #Cadena donde se guarda lo recien leido del buffer de
             #entrada
             entrada = ""
-
             ultimo_char=self.stream.read(1)
-            
-            #Lee caracter a caracter hasta que encuentra el delimitador
-            while ( ultimo_char != self.dep ):
-                entrada+=self.stream.read(1)
-                ultimo_char=self.stream.read(1)
 
+
+            #Lee caracter a caracter hasta que encuentra el delimitador
+            while ( ultimo_char != self.sep ):
+                entrada+=ultimo_char
+                ultimo_char=self.stream.read(1)
+            
             if ( len(entrada)>0 ) :
-                medida = self.traducir( dato )
-                self.lista.append( medida )
+                medida = self.traducir( entrada )
+                #self.lista.put( medida )
+                self.volcar_datos(medida)
+        #Esto se ejecutara cuando finalize
+        pass
