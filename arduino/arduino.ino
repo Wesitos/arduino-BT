b2b097b968d51acdc4d837d6c15ca8ddc8f3868a
Roy Prosopio <roy_prosopio_4@hotmail.com>
2014-02-23 19:45:54 -0500

    primer prototipo arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,14 +1,13 @@
-//Programa del arduino
+// proyecto de tesis abraham
+String A;
 int numero=0;
-char A[26];int T[5]={0,0,0,0,0}; 
-int T1=0, T2=0, T3=0, T4=0, T5=0;
-int pulso=1;
-String S1,S2,S3,S4,S5;
-String C="";
-int indice=0; 
-void setup()
+String S,S1;
+int T[150];
+int t;
+int indice=0;
+void setup() 
 {
-  Serial.begin(9600);
+   Serial.begin(9600);
   
   pinMode(13,OUTPUT);//pin de alimentacion al sensor de temperatura
   pinMode(12,OUTPUT); //pines del bluetooth
@@ -17,38 +16,41 @@ void setup()
   digitalWrite(12, HIGH);
   digitalWrite(13, LOW);
   digitalWrite(8,0);
+
 }
-void loop()
+
+void loop() 
 {
-  //Lee cuantos bytes puede leer
   int rec=Serial.available();
-    
-  if(rec>2)
-    conf_tiempo();
-
-  if(numero>2)
-    { 
-      String mm="";
-      for(int i=0;i<5;i++)
-	if( indice % T[i]==0 && T[i]!=0)
-	  mm=mm+lectura(i+1);
-
-      mm=mm;
-      if(mm.length()>1)
-	Serial.print(mm);
-
-      indice=indice+1;
-    }; 
-  delay(1000);
+       if(rec>2)
+       {
+          config_tiempo(); 
+       } 
+       if(numero>2)
+       {
+         String mm="";
+         for(int j=0;j<t;j++)
+         {
+         if( indice % T[j]==0 && T[j]!=0)
+           {
+	      mm=mm+lectura(j+1);
+           } 
+         } 
+         if(mm.length()>1)
+         {
+         Serial.print(mm);
+         }
+         indice++;        
+       } 
 }
 
-//-----funciones------
-     
 String lectura(int n)
 {
-  String x;int a=0; int dato=0;
+  String x;
+  int a=0; 
+  int dato=0;
   switch(n)
-    {
+    {//esto va a depender de la cantidad de sensores utilizados
     case 1:
       //TIEMPO DE MUESTREO DEL SENSOR 
       digitalWrite(13, HIGH);delay(9); dato=filtrador(0);digitalWrite(13, LOW);
@@ -73,7 +75,6 @@ String lectura(int n)
   return x;
 }
 
-
 int filtrador(int n)
 {
   long b=0, c=0, a=0;
@@ -81,29 +82,39 @@ int filtrador(int n)
     {
       a=analogRead(n);
       b=b+a;
-    };
-  c=int(b/1000);return c;
+    }
+  c=int(b/1000);
+  return c;
 }
 
-void conf_tiempo()
+
+void config_tiempo()
 {
-  // el buffer entrante es de la forma "120005465200343211223"
-  numero=Serial.readBytes(A,25);
-    
-  //Los tiempos maximos son un dia     
-  S1=String(A).substring(0,5);// separamos TIEMPO1
-  T1=S1.toInt();//eliminamos los ceros
-    
-  S2=String(A).substring(5,10);// separamos TIEMPO2
-  T2=S2.toInt();//eliminamos los ceros
-    
-  S3=String(A).substring(10,15);// separamos TIEMPO3
-  T3=S3.toInt();//eliminamos los ceros
-    
-  S4=String(A).substring(15,20);// separamos TIEMPO4
-  T4=S4.toInt();//eliminamos los ceros
-    
-  S5=String(A).substring(20,25);// separamos TIEMPO5
-  T5=S5.toInt();
-  T[0]=T1;T[1]=T2;T[2]=T3;T[3]=T4;T[4]=T5;
+  //queremos que el buffer entrante sea de la forma "26362;15655;55453;43544;52562;46591#"
+         
+        numero=Serial.readBytes(A);
+        int i;
+        int puntoycoma=0;
+        int primer_puntoycoma=A.indexOf(';');
+        int primer_michi=A.indexof('#');
+        for(i=0;;i++)
+        {
+          S=String(A).substring(puntoycoma,primer_puntoycoma-1);//separamos TIEMPO1
+          T[i]=S.toInt();
+          puntoycoma=primer_puntoycoma;
+          primer_puntoycoma=A.indexOf(';',primer_puntoycoma+1);
+          S1=S1+S;
+        } 
+       t=T.length();
+}
+void separador()
+{
+ int n_inicial=-1;
+ int i=0;
+ do
+ {
+ 	n_inicial=A.indexof('#',n_inicial+1);
+ 	i++;
+ }while(n_inicial>=0)
+
 }
